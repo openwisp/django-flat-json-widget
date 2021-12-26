fd10da9ff112a0e8d580c8c496ee2c0ff08cc1dc
Harsh Kumar <54139847+harsh-9in@users.noreply.github.com>
2021-12-27 01:50:40 +0530

    [deps] Added support for Django 3.2 and Django 4.0a #11
    Test dependencies:
    - Bumped django-extensions~=3.1.0
    - Bumped openwisp-utils to use latest master from GitHub
    
    Closes #11

--- tests/urls.py
+++ tests/urls.py
@@ -1,11 +1,11 @@
 from django.conf import settings
-from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
@@ -14,4 +14,4 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG and 'debug_toolbar' in settings.INSTALLED_APPS:
     import debug_toolbar
 
-    urlpatterns += [url(r'^__debug__/', include(debug_toolbar.urls))]
+    urlpatterns += [path('^__debug__/', include(debug_toolbar.urls))]
